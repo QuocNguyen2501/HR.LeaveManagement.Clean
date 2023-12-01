15c07e84d739792278c536e386bf5550f919e224
quocng <>
2023-12-01 16:36:25 +0700

    init project

---
+++ HR.LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿namespace HR.LeaveManagement.Domain
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+    }
+}
