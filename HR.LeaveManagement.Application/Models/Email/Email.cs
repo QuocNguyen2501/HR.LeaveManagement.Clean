814e200f72a5bb4c1fca6f0c99a86f997609e226
quocng <>
2023-12-04 15:53:12 +0700

    add email service

---
+++ HR.LeaveManagement.Application/Models/Email/Email.cs
@@ -0,0 +1,8 @@
+﻿namespace HR.LeaveManagement.Application.Models.Email;
+
+public class EmailMessage
+{
+    public List<string> Tos { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+}
---
+++ HR.LeaveManagement.Infrastructure/EmailService/EmailSender.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Models.Email;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace HR.LeaveManagement.Infrastructure.EmailService;
+
+public class EmailSender : IEmailSender
+{
+    public EmailSetting _emailSettings { get; }
+
+    public EmailSender(IOptions<EmailSetting> emailSettings)
+    {
+        _emailSettings = emailSettings.Value;   
+    }
+    public async Task<bool> SendEmail(EmailMessage email)
+    {
+        var client = new SendGridClient(_emailSettings.ApiKey);
+        List<EmailAddress> tos = new List<EmailAddress>();
+
+        email.Tos.ForEach(to =>
+        {
+            tos.Add(new EmailAddress(to));
+        });
+
+        var from = new EmailAddress
+        {
+            Email = _emailSettings.FromAddress,
+            Name = _emailSettings.FromName
+        };
+        var message = MailHelper.CreateSingleEmailToMultipleRecipients(from, tos, email.Subject, email.Body, email.Body);
+        var response = await client.SendEmailAsync(message);
+
+        return response.StatusCode == HttpStatusCode.OK || response.StatusCode == HttpStatusCode.Accepted;
+    }
+}
