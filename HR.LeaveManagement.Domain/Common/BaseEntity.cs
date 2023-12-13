c1842d7b110fea6a524abc278d53efaa169d2ad1
quocng <>
2023-12-13 15:00:40 +0700

    add blazor project

--- HR.LeaveManagement.Domain/Common/BaseEntity.cs
+++ HR.LeaveManagement.Domain/Common/BaseEntity.cs
@@ -4,7 +4,7 @@ namespace HR.LeaveManagement.Domain.Common;
 public abstract class BaseEntity
 {
 	public string Id { get; set; }
-	public DateTime DateCreated { get; set; } = new DateTime();
-	public DateTime DateModified { get; set; } = new DateTime();
+	public DateTime? DateCreated { get; set; }
+	public DateTime? DateModified { get; set; }
 }
 
---
+++ HR.LeaveManagement.Persistence.IntegrationTests/HrDatabaseContextTest.cs
@@ -0,0 +1,50 @@
+﻿using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Shouldly;
+
+namespace HR.LeaveManagement.Persistence.IntegrationTests
+{
+    public class HrDatabaseContextTest
+    {
+        private readonly HrDatabaseContext _hrDatabaseContext;
+        public HrDatabaseContextTest()
+        {
+            var dbOptions = new DbContextOptionsBuilder<HrDatabaseContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+            _hrDatabaseContext = new HrDatabaseContext(dbOptions);
+        }
+
+        [Fact]
+        public async void Save_SetDateCreatedValue()
+        {
+            // Arrange
+            var leaveType = new LeaveType
+            {
+                Id = "b80194ce-25f3-41ed-900f-2bfa801e2a81",
+                DefaultDays = 15,
+                Name = "Test Sick"
+            };
+            // Act
+            await _hrDatabaseContext.LeaveTypes.AddAsync(leaveType);
+            await _hrDatabaseContext.SaveChangesAsync();
+            // Assert
+            leaveType.DateCreated.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public async void Save_SetDateModifiedValue() {
+            var leaveType = new LeaveType
+            {
+                Id = "b80194ce-25f3-41ed-900f-2bfa801e2a81",
+                DefaultDays = 15,
+                Name = "Test Sick"
+            };
+            // Act
+            await _hrDatabaseContext.LeaveTypes.AddAsync(leaveType);
+            await _hrDatabaseContext.SaveChangesAsync();
+            // Assert
+            leaveType.DateModified.ShouldNotBeNull();
+        }
+    }
+}
